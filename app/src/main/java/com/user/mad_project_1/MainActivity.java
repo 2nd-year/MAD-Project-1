ac1a42d75ccdd90bcf32ebe93108d694b7ef6b43
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-03-17 11:07:43 +0530

    add intent

--- app/src/main/java/com/user/mad_project_1/MainActivity.java
+++ app/src/main/java/com/user/mad_project_1/MainActivity.java
@@ -2,15 +2,32 @@ package com.user.mad_project_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+      Button signBtn = findViewById(R.id.signInBtn);
+
+
+       signBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
