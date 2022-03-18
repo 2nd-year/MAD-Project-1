2ac5b4a2164152ba234efcf15a884a409c474696
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-03-18 10:33:21 +0530

    user profile activity

---
+++ app/src/main/java/com/user/mad_project_1/Profile.java
@@ -0,0 +1,16 @@
+package com.user.mad_project_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
